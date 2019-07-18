9b7bd9381bd0a0709998346ca9702e5c16f2807a
Michał Szczepaniak <49671638+MichalSz99@users.noreply.github.com>
2019-07-18 15:05:04 +0200

    Update SelectionSort.py

--- SortingAlgorithms/SelectionSort.py
+++ SortingAlgorithms/SelectionSort.py
@@ -1,12 +1,13 @@
-def SelectionSort(A):
-    for i in range(len(A)-1, 0, -1):
-        maks = i
+def selection_sort(arr):
+    for i in range(len(arr)-1, 0, -1):
+        max_index = i
         for j in range(i, -1, -1):
-            if A[j] > A[maks]:
-                maks = j
-        A[maks], A[i] = A[i], A[maks]
-    return A
+            if arr[j] > arr[max_index]:
+                max_index = j
+        arr[max_index], arr[i] = arr[i], arr[max_index]
+    return arr
 
 
-Tablica = [1, 4, 6, 4, 7, 36, 346, 3, 57, 34, -501, 37]
-print(SelectionSort(Tablica))
+array = [1, 4, 6, 4, 7, 36, 346, 3, 57, 34, -501, 37]
+print(selection_sort(array))
+ 
