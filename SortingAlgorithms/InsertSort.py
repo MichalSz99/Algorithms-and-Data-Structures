49925a396661e08fe59ea0123cdc49812c8126b6
Michał Szczepaniak <49671638+MichalSz99@users.noreply.github.com>
2019-07-18 14:52:10 +0200

    Update InsertSort.py

--- SortingAlgorithms/InsertSort.py
+++ SortingAlgorithms/InsertSort.py
@@ -1,12 +1,14 @@
-def InsertSort (A):
-     for i in range(1,len(A)):
-        key = A[i]
-        j=i-1
-        while j >= 0 and  A[j] > key:
-            A[j+1] = A[j]
-            j -=1
-        A[j+1]=key
-     return A
+def insert_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
 
-Tablica = [1,4,6,4,7,36,346,3,57,34,-50,37]
-print(InsertSort(Tablica))
+
+array = [1, 4, 6, 4, 7, 36, 346, 3, 57, 34, -50, 37]
+print(insert_sort(array))
+ 
