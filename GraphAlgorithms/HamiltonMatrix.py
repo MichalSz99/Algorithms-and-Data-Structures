cc415f3f6c36dc2c33109b374b04683e8e4d386a
MichalSz99 <m.szczepaniak99@gmail.com>
2019-07-18 16:29:23 +0200

    Update HamiltonMatrix.py

--- GraphAlgorithms/HamiltonMatrix.py
+++ GraphAlgorithms/HamiltonMatrix.py
@@ -25,16 +25,16 @@ class Graph:
             for j in range(self.vertex):
                 self.matrix[i][j] = 0
 
-    def randomFill(self, density):
+    def random_fill(self, density):
         self.clear()
-        MAXedge = round(self.vertex * (self.vertex - 1) * (density / 100))
+        max_edge = round(self.vertex * (self.vertex - 1) * (density / 100))
         for i in range(self.vertex - 1):
             self.matrix[i][i + 1] = 1
             self.matrix[i + 1][i] = 1
         self.matrix[0][self.vertex - 1] = 1
         self.matrix[self.vertex - 1][0] = 1
         count = 2 * self.vertex 
-        while count < MAXedge:
+        while count < max_edge:
             x = random.randint(0, self.vertex - 1)
             y = random.randint(0, self.vertex - 1)
             if x == y or self.matrix[x][y] == 1:
@@ -43,7 +43,7 @@ class Graph:
                 self.matrix[x][y] = 1
                 self.matrix[y][x] = 1
                 count += 2
-        for i in range(self.vertex):
+        for j in range(self.vertex):
             x = random.randint(0, self.vertex - 1)
             y = random.randint(0, self.vertex - 1)
             if x != y:
@@ -51,45 +51,45 @@ class Graph:
                 for i in range(self.vertex):
                     self.matrix[i][x], self.matrix[i][y] = self.matrix[i][y], self.matrix[i][x]
 
-    def fromFile(self, fileName):
+    def from_file(self, file_name):
         self.clear()
-        file = open(fileName, "r")
-        input = file.read()
-        vertex = re.findall(r"\d+", input)
-        for i in range(0, len(vertex), 2):
-            predecessor = int(vertex[i])
-            successor = int(vertex[i + 1])
+        file = open(file_name, "r")
+        input_data = file.read()
+        data = re.findall(r"\d+", input_data)
+        for i in range(0, len(data), 2):
+            predecessor = int(data[i])
+            successor = int(data[i + 1])
             self.matrix[predecessor][successor] = 1
             self.matrix[successor][predecessor] = 1
 
-    def __Hamilton(self, out, startVertex, currentVertex, end):
+    def __hamilton(self, out, start_vertex, current_vertex, end):
         if not end:
             for i in range(len(self.matrix)):
-                if self.matrix[currentVertex][i] == 1 and not (i in out):
+                if self.matrix[current_vertex][i] == 1 and not (i in out):
                     out.append(i)
-                    out, end = self.__Hamilton(out, startVertex, i, end)
+                    out, end = self.__hamilton(out, start_vertex, i, end)
                     if end:
                         return out, end
-            if len(out) == self.vertex and self.matrix[currentVertex][startVertex] == 1:
-                out.append(startVertex)
+            if len(out) == self.vertex and self.matrix[current_vertex][start_vertex] == 1:
+                out.append(start_vertex)
                 end = True
             else:
                 out.pop()
         return out, end
 
-    def Hamilton(self, startVertex):
-        out = [startVertex]
+    def hamilton(self, start_vertex):
+        out = [start_vertex]
         end = False
-        out, end = self.__Hamilton(out, startVertex, startVertex, end)
+        out, end = self.__hamilton(out, start_vertex, start_vertex, end)
         return out
 
 
 graph = Graph(10)
-graph.randomFill(30)
+graph.random_fill(30)
 print(graph)
 print("\n")
-print(graph.Hamilton(0))
-graph.fromFile("HamiltonData.txt")
+print(graph.hamilton(0))
+graph.from_file("HamiltonData.txt")
 print(graph)
 print("\n")
-print(graph.Hamilton(0))
+print(graph.hamilton(0))
