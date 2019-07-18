7210f6f59fd0ae8cd10b5f8ad9835fbe8703c552
Michał Szczepaniak <49671638+MichalSz99@users.noreply.github.com>
2019-07-18 15:32:20 +0200

    Update QuickSortI.py

--- SortingAlgorithms/QuickSortI.py
+++ SortingAlgorithms/QuickSortI.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Stacks:
     def __init__(self):
         self.tab = []
@@ -8,8 +9,8 @@ class Stacks:
         self.tab.append((a, b))
 
     def pop(self):
-       pom = self.tab.pop(-1)
-       return pom
+        pom = self.tab.pop(-1)
+        return pom
 
     def empty(self):
         if len(self.tab) > 0:
@@ -17,25 +18,26 @@ class Stacks:
         else:
             return True
 
-def QuickSort(A):
+
+def quick_sort(arr):
     stack = Stacks()
-    stack.push(0, len(A)-1)
+    stack.push(0, len(arr)-1)
     while not stack.empty():
         l, r = stack.pop()
-        while l < r:
-           x = random.randint(l,r)
-           pivot = A[x]
-           A[r], A[x] = A[x], A[r]
-           i = l
-           for j in range(l,r):
-                if A[j] <= pivot:
-                     A[i], A[j] = A[j], A[i]
-                     i +=1
-           A[i], A[r] = A[r], A[i]
-           if i<r:
-                stack.push(i, r)
-           r = j
-    return A
+        if l < r:
+            x = random.randint(l, r)
+            pivot = arr[x]
+            arr[r], arr[x] = arr[x], arr[r]
+            i = l
+            for j in range(l, r):
+                if arr[j] <= pivot:
+                    arr[i], arr[j] = arr[j], arr[i]
+                    i += 1
+            arr[i], arr[r] = arr[r], arr[i]
+            stack.push(l, i-1)
+            stack.push(i+1, r)
+    return arr
 
-Tablica = [1, 4, 6, 7, 36, 346, 3, 57, 34, -503, 37,3,1,4,6]
-print(QuickSort(Tablica))
+
+array = [1, 4, 6, 7, 36, 346, 3, 57, 34, -503, 37, 3, 1, 4, 6]
+print(quick_sort(array))
